f8c275b2db077e821406740f13c6189efa743b79
Mohamed amine <mohamedaminemounir7@gmail.com>
2024-11-13 14:35:58 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/menu" element={<Menu />} />
-          <Route path="/reservation" element={<Reservation bookingLink="https://www.booking.com/hotel/ma/borj-el-arab.fr.html" />} />
+          <Route path="/reservation" element={<Reservation bookingLink="https://www.booking.com/index.en-gb.html?aid=2336990;label=en-ma-booking-desktop-q4cK3TQcnarHAo7x8L6CyAS652804038404:pl:ta:p1:p2:ac:ap:neg:fi:tikwd-33467740:lp1029469:li:dec:dm;ws=&gclid=CjwKCAiAudG5BhAREiwAWMlSjNPuKrgNoxErAM7j_lvFiG5D2WqEYYACR5ql6eAZTtOz_dgihHvK8RoCyMgQAvD_BwE" />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/hotel" element={<Hotel />} />
           <Route path="/fitness" element={<Fitness />} />
